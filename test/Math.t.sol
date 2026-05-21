544537b13042650cbbcfd9058c09a405ecebe985
0xundesk <283016779+0xundesk@users.noreply.github.com>
2026-05-21 12:10:12 +0000

    test: ln against ln1 ln2 ln10 ln0.5

--- test/Math.t.sol
+++ test/Math.t.sol
@@ -16,6 +16,13 @@ contract MathTest is Test {
         near(int256(Fixed.sqrt(9e18)), 3e18, 1e6, "sqrt9");
     }
 
+    function test_Ln() public pure {
+        near(Fixed.ln(1e18), 0, 1e6, "ln1");
+        near(Fixed.ln(2e18), 693147180559945309, 1e8, "ln2");
+        near(Fixed.ln(10e18), 2302585092994045684, 1e8, "ln10");
+        near(Fixed.ln(5e17), -693147180559945309, 1e8, "ln0.5");
+    }
+
     function test_Exp() public pure {
         near(Fixed.exp(0), 1e18, 1e6, "exp0");
         near(Fixed.exp(1e18), 2718281828459045235, 1e9, "exp1");
@@ -24,4 +31,3 @@ contract MathTest is Test {
     }
 
 }
-
